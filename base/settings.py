07ab46f4fcb47a7e2bf8d08d5d64a1ab637f0023
solomon <solomong1331@gmail.com>
2024-05-19 21:44:41 +0530

    new datase

--- base/settings.py
+++ base/settings.py
@@ -86,15 +86,31 @@ WSGI_APPLICATION = 'base.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 import dj_database_url
 
-DATABASES = dj_database_url.config(
-    conn_max_age=600,
-    conn_health_checks=True,
-)
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
+
+
+# import dj_database_url
+
+# DATABASES = dj_database_url.config(
+#     conn_max_age=600,
+#     conn_health_checks=True,
+# )
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
